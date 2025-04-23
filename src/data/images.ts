e97f08bb3b196258eb670e8737e29a541b24637a
gideok Kim <ikid4127@naver.com>
2025-04-23 09:46:18 +0900

    🎨 Add images

--- src/data/images.ts
+++ src/data/images.ts
@@ -6,6 +6,27 @@ export const images: CatImage[] = [
     title: 'My Cat',
     description: 'My beautiful cat',
     imageUrl: '/images/cat1.jpg',
-    date: '2024-03-20'
+    date: '2025-03-20'
+  },
+  {
+    id: '2',
+    title: '엄마 일하지 말라고 방해하는 다미',
+    description: '엄마 일하지 말라고 방해하는 다미',
+    imageUrl: '/images/cat2.jpg',
+    date: '2025-03-20'
+  },
+  {
+    id: '3',
+    title: '이불 덮고 자는 다미',
+    description: '이불 덮고 자는 다미',
+    imageUrl: '/images/cat3.jpg',
+    date: '2025-03-20'
+  },
+  {
+    id: '4',
+    title: '친구랑 같이 노는 다미',
+    description: '친구랑 같이 노는 다미',
+    imageUrl: '/images/cat4.jpg',
+    date: '2025-03-20'
   }
-]; 
+];
