567047d3fef36991c75fd893341a7aa8a3b73bc8
gideok Kim <ikid4127@naver.com>
2025-04-23 10:11:27 +0900

    🎨 Modify file name

--- src/data/images.ts
+++ src/data/images.ts
@@ -3,30 +3,37 @@ import { CatImage } from '@/types';
 export const images: CatImage[] = [
   {
     id: '1',
-    title: 'My Cat',
-    description: 'My beautiful cat',
-    imageUrl: '/images/cat1.jpg',
-    date: '2025-03-20'
+    title: '이불 덮고 자는 다미',
+    description: '이불 덮고 자는 다미',
+    imageUrl: '/images/2025-04-07-dami01.jpg',
+    date: '2025-04-07'
   },
   {
     id: '2',
-    title: '엄마 일하지 말라고 방해하는 다미',
-    description: '엄마 일하지 말라고 방해하는 다미',
-    imageUrl: '/images/cat2.jpg',
-    date: '2025-03-20'
+    title: '친구랑 같이 노는 다미',
+    description: '친구랑 같이 노는 다미',
+    imageUrl: '/images/2025-04-07-dami02.jpg',
+    date: '2025-04-07'
   },
   {
     id: '3',
-    title: '이불 덮고 자는 다미',
-    description: '이불 덮고 자는 다미',
-    imageUrl: '/images/cat3.jpg',
-    date: '2025-03-20'
+    title: '엄마 일하지 말라고 방해하는 다미',
+    description: '엄마 일하지 말라고 방해하는 다미',
+    imageUrl: '/images/2025-04-00-dami.jpg',
+    date: '2025-04-08'
   },
   {
     id: '4',
-    title: '친구랑 같이 노는 다미',
-    description: '친구랑 같이 노는 다미',
-    imageUrl: '/images/cat4.jpg',
-    date: '2025-03-20'
+    title: '엄마를 보는 다미',
+    description: '엄마를 보는 다미',
+    imageUrl: '/images/2025-04-14-dami.jpg',
+    date: '2025-04-14'
+  },
+  {
+    id: '5',
+    title: '이불 위에서 졸고 있는 다미',
+    description: '이불 위에서 졸고 있는 다미',
+    imageUrl: '/images/2025-04-22-dami.jpg',
+    date: '2025-04-22'
   }
 ];
