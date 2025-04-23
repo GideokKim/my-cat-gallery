53e543920b0356a59ca3ec50b4be3200a1809641
gideok Kim <ikid4127@naver.com>
2025-04-23 14:13:06 +0900

    🎨 Add images

--- src/data/images.ts
+++ src/data/images.ts
@@ -31,8 +31,36 @@ export const images: CatImage[] = [
   },
   {
     id: '5',
-    title: '이불 위에서 졸고 있는 다미',
-    description: '이불 위에서 졸고 있는 다미',
+    title: '이불 위에서 자고 있는 다미',
+    description: '이불 위에서 자고 있는 다미',
+    imageUrl: '/images/2025-04-17-dami01.jpg',
+    date: '2025-04-17'
+  },
+  {
+    id: '6',
+    title: '귀찮은 다미',
+    description: '귀찮은 다미',
+    imageUrl: '/images/2025-04-17-dami02.jpg',
+    date: '2025-04-17'
+  },
+  {
+    id: '7',
+    title: '장난감 위에 누워있는 다미',
+    description: '장난감 위에 누워있는 다미',
+    imageUrl: '/images/2025-04-19-dami01.jpg',
+    date: '2025-04-19'
+  },
+  {
+    id: '8',
+    title: '이상한 자세로 구경하는 다미',
+    description: '이상한 자세로 구경하는 다미',
+    imageUrl: '/images/2025-04-19-dami02.jpg',
+    date: '2025-04-19'
+  },
+  {
+    id: '9',
+    title: '졸고 있는 다미',
+    description: '졸고 있는 다미',
     imageUrl: '/images/2025-04-22-dami.jpg',
     date: '2025-04-22'
   }
